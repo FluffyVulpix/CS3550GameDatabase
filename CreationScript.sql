3d02bd091e608f9204abe4bcbba605d3e29400a6
Shesh <smeldrum18@live.com>
2019-10-02 11:06:20 -0600

    Whitespace changes

--- CreationScript.sql
+++ CreationScript.sql
@@ -6,39 +6,51 @@ END
 --Drop all tables if they exist in the database already
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'GamesConsoles')
     DROP TABLE GameDatabase.GamesConsoles;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'GamesEditions')
     DROP TABLE GameDatabase.GamesEditions;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Platforms')
     DROP TABLE GameDatabase.Platforms;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Consoles')
     DROP TABLE GameDatabase.Consoles;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'GameGenres')
     DROP TABLE GameDatabase.GameGenres;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Genres')
     DROP TABLE GameDatabase.Genres;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'GamePublishers')
     DROP TABLE GameDatabase.GamePublishers;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Publishers')
     DROP TABLE GameDatabase.Publishers;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'GameDevelopers')
     DROP TABLE GameDatabase.GameDevelopers;
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Developers')
     DROP TABLE GameDatabase.Developers
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Games')
     DROP TABLE GameDatabase.Games
+;
 
 IF EXISTS (SELECT 1 FROM sys.tables WHERE Name = 'Regions')
     DROP TABLE GameDatabase.Regions
+;
 
 
 --Regions Table Creation
@@ -49,7 +61,6 @@ CREATE TABLE GameDatabase.Regions
     CreationDate datetime2 DEFAULT(getdate()) NOT NULL
 )
 
-
 --Games Table Creation
 CREATE TABLE GameDatabase.Games
 (
@@ -59,7 +70,6 @@ CREATE TABLE GameDatabase.Games
     ESRB nvarchar(4) NOT NULL,
     CreationDate datetime2 DEFAULT(getdate()) NOT NULL
 )
-
 
 --GamesEditions Table Creation
 CREATE TABLE GameDatabase.GamesEditions
@@ -77,7 +87,7 @@ ALTER TABLE GameDatabase.GamesEditions
         REFERENCES GameDatabase.Regions (RegionID),
     CONSTRAINT fk_GamesEditions_GameID FOREIGN KEY (GameID)
         REFERENCES GameDatabase.Games (GameID);
-
+;
 
 --Platforms Table Creation
 CREATE TABLE GameDatabase.Platforms
@@ -90,6 +100,7 @@ CREATE TABLE GameDatabase.Platforms
 ALTER TABLE GameDatabase.Platforms
     ADD CONSTRAINT fk_Platforms_RegionID FOREIGN KEY (RegionID)
         REFERENCES GameDatabase.Regions (RegionID);
+;
 
 --Consoles Table Creation
 CREATE TABLE GameDatabase.Consoles
@@ -112,6 +123,7 @@ ALTER TABLE GameDatabase.GamesConsoles
         REFERENCES GameDatabase.GamesEditions (GameEditionID),
     CONSTRAINT fk_GamesConsoles_PlatformID FOREIGN KEY (PlatformID)
         REFERENCES GameDatabase.Platforms (PlatformID);
+;
 
 --Developers Table Creation
 CREATE TABLE GameDatabase.Developers
@@ -134,6 +146,7 @@ ALTER TABLE GameDatabase.GameDevelopers
         REFERENCES GameDatabase.Games (GameID),
     CONSTRAINT fk_GameDevelopers_DeveloperID FOREIGN KEY (DeveloperID)
         REFERENCES GameDatabase.Developers (DeveloperID)
+;
 
 ----Modify
 --Publishers Table Creation
@@ -158,6 +171,7 @@ ALTER TABLE GameDatabase.GamePublishers
             REFERENCES GameDatabase.Games (GameID),
         CONSTRAINT fk_GamePublishers_PublisherID FOREIGN KEY (PublisherID)
             REFERENCES GameDatabase.Publishers (PublisherID)
+;
 
 --Genres Table Creation
 CREATE TABLE GameDatabase.Genres
@@ -180,3 +194,4 @@ ALTER TABLE GameDatabase.GameGenres
             REFERENCES GameDatabase.Games (GameID),
         CONSTRAINT fk_GameGenres_GenreID FOREIGN KEY (GenreID)
             REFERENCES GameDatabase.Genres (GenreID)
+;
